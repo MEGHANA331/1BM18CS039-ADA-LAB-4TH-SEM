fb8251f02ece50475175e06d789025b4f871d315
MEGHANA331 <53857407+MEGHANA331@users.noreply.github.com>
2020-02-19 21:45:16 +0530

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -49,3 +49,65 @@ bubble_sort(a,n);
 }
 
 output:
+
+enter the size of the array                                                                                                                     
+5                                                                                                                                               
+enter the elements of the array                                                                                                                 
+79                                                                                                                                              
+47                                                                                                                                              
+76                                                                                                                                              
+10                                                                                                                                              
+99                                                                                                                                              
+sorted array is :                                                                                                                               
+10                                                                                                                                              
+47                                                                                                                                              
+76                                                                                                                                              
+79                                                                                                                                              
+99                                                                                                                                              
+time taken is 0.000277  
+  
+  
+  enter the size of the array                                                                                                                     
+5                                                                                                                                               
+enter the elements of the array                                                                                                                 
+90                                                                                                                                              
+38                                                                                                                                              
+46                                                                                                                                              
+58                                                                                                                                              
+54                                                                                                                                              
+sorted array is :                                                                                                                               
+38                                                                                                                                              
+46                                                                                                                                              
+54                                                                                                                                              
+58                                                                                                                                              
+90                                                                                                                                              
+time taken is 0.000343
+  
+  enter the size of the array                                                                                                                     
+10                                                                                                                                              
+enter the elements of the array                                                                                                                 
+1000                                                                                                                                            
+46                                                                                                                                              
+29                                                                                                                                              
+5678                                                                                                                                            
+20                                                                                                                                              
+999                                                                                                                                             
+456                                                                                                                                             
+783                                                                                                                                             
+657                                                                                                                                             
+209                                                                                                                                             
+sorted array is :                                                                                                                               
+20                                                                                                                                              
+29                                                                                                                                              
+46                                                                                                                                              
+209                                                                                                                                             
+456                                                                                                                                             
+657                                                                                                                                             
+783                                                                                                                                             
+999                                                                                                                                             
+1000                                                                                                                                            
+5678                                                                                                                                            
+time taken is 0.000408  
+  
+  
+  
