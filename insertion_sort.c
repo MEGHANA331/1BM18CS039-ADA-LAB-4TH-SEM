ada84b65dec3130b9dbf07be072f3ad7bd9b79bb
MEGHANA331 <53857407+MEGHANA331@users.noreply.github.com>
2020-03-20 19:19:54 +0530

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,34 +1,42 @@
-
-#include <math.h> 
-#include <stdio.h> 
-  
-
-void insertionSort(int arr[], int n) 
-{ 
-int i, key, j; 
-for (i = 1; i < n; i++) { 
-key = arr[i]; 
-j = i - 1;         
-while (j >= 0 && arr[j] > key) 
-{ 
-arr[j + 1] = arr[j]; 
-j = j - 1; 
-} 
-arr[j + 1] = key; 
-} 
-} 
-void printArray(int arr[], int n) 
-{ 
-int i; 
-for (i = 0; i < n; i++) 
-printf("%d ", arr[i]); 
-printf("\n"); 
-} 
-int main() 
-{ 
-int arr[] = { 12, 11, 13, 5, 6 }; 
-int n = sizeof(arr) / sizeof(arr[0]); 
-insertionSort(arr, n); 
-printArray(arr, n); 
-return 0; 
-} 
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void select(int a[],int n)
+{
+int i,j,small,pos,item,k;
+for(i=0;i<n;i++)
+{
+item=a[i];
+j=i-1;
+while(j>=0&&a[j]>item)
+{
+a[j+1]=a[j];
+j=j-1;
+}
+a[j+1]=item;
+}
+printf("sorted array");
+for(i=0;i<n;i++)
+{
+printf("%d\n",a[i]);
+}
+}
+int main()
+{
+int n,i,j,small,pos,temp;
+printf("enter the number of elements\n");
+scanf("%d",&n);
+int a[n];
+printf("enter the elements\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+clock_t t; 
+t=clock();
+select(a,n); 
+t = clock() - t; 
+double time_taken = ((double)t)/CLOCKS_PER_SEC; 
+printf("sort function took %f seconds to execute \n", time_taken); 
+return 0;
+}
